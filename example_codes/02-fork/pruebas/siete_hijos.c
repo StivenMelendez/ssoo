e8de09a0497ff66b787609fea7f8c025867b1168
StivenMelendez <stivenrafael@hotmail.com>
2025-03-16 22:26:57 -0500

    Refactor la creación de procesos en el código de prueba para manejar hijos de manera más estructurada y esperar su finalización.

--- example_codes/02-fork/pruebas/siete_hijos.c
+++ example_codes/02-fork/pruebas/siete_hijos.c
@@ -5,37 +5,50 @@
 
 int main(){
     printf("soy el PADRE, el proceso inicio: \n\n");
-    if(fork()==0){
+
+    pid_t pid1, pid2, pid3;
+
+    if((pid1 = fork()) == 0){
         printf("soy el hijo_1\n\n");
-        if(fork()==0){
+        pid_t pid4, pid5;
+        if((pid4 = fork()) == 0){
             printf("soy el hijo_4\n\n");
             exit(EXIT_SUCCESS);
-        }else if(fork()==0){
+        }else if((pid5 = fork()) == 0){
             printf("soy el hijo_5\n\n");
             exit(EXIT_SUCCESS);
         }else{
+            waitpid(pid4, NULL, 0);
+            waitpid(pid5, NULL, 0);
             exit(EXIT_SUCCESS);
         }
     }
 
-    if(fork()==0){
+    if((pid2 = fork()) == 0){
         printf("soy el hijo_2\n\n");
-        if(fork()==0){
+        pid_t pid6, pid7;
+        if((pid6 = fork()) == 0){
             printf("soy el hijo_6\n\n");
             exit(EXIT_SUCCESS);
-        }else if(fork()==0){
+        }else if((pid7 = fork()) == 0){
             printf("soy el hijo_7\n\n");
             exit(EXIT_SUCCESS);
         }else{
+            waitpid(pid6, NULL, 0);
+            waitpid(pid7, NULL, 0);
             exit(EXIT_SUCCESS);
         }
     }
 
-    if(fork()==0){
+    if((pid3 = fork()) == 0){
         printf("soy el hijo_3\n\n");
         exit(EXIT_SUCCESS);
     }
-    wait(NULL); //
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    waitpid(pid3, NULL, 0);
+
     printf("soy el PADRE, el proceso finalizo\n\n");
     return 0;
 }
